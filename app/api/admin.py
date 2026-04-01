d1a94516b7ecfa672262c6a82714bff71853523d
Tim Ashton <tim.ashton.la@gmail.com>
2026-03-31 22:21:25 -0700

    Flake8 fixes

--- app/api/admin.py
+++ app/api/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from .models import Player
 
+
 # Register your models here.
 @admin.register(Player)
 class PersonAdmin(admin.ModelAdmin):
-    list_display = ['handle','game_id']
+    list_display = ['handle', 'game_id']
